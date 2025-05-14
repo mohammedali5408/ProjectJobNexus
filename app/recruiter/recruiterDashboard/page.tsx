7111a3f63f9b0ac6ac54a43649fb5eed4ddb0274
Mohammed Ali <helloalimohammed@gmail.com>
2025-05-13 21:00:17 -0500

    Update page.tsx

--- app/recruiter/recruiterDashboard/page.tsx
+++ app/recruiter/recruiterDashboard/page.tsx
@@ -532,15 +532,6 @@ const calculateMatchScore = (applicantId: string, position: string): string => {
             </svg>
             Find Candidates
           </Link>
-          <Link 
-            href="/recruiter/analytics" 
-            className="px-6 py-3 bg-white text-indigo-600 border border-indigo-200 rounded-lg flex items-center justify-center hover:bg-indigo-50 transition-colors"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-            </svg>
-            View Analytics
-          </Link>
         </motion.div>
         
         {/* Tabs */}
